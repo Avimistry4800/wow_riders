187dfdf7cfa5ebe4d4fe5556401610658ff05167
Avimistry4800 <avimistry4800@gmail.com>
2021-03-19 13:09:19 +0600

    working on it

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjNgcWe7V0mhyrZW9LKapsR8J5NqrGN7E",
+    authDomain: "wow-riders.firebaseapp.com",
+    projectId: "wow-riders",
+    storageBucket: "wow-riders.appspot.com",
+    messagingSenderId: "179377762903",
+    appId: "1:179377762903:web:07a54f9bf906b5882f2912"
+  };
+  export default firebaseConfig;
